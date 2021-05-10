c5cb8d2c441efa399d3b800befc26d36ff36153f
Hovak Davtyan <hovak.stdev@gmail.com>
2021-05-10 16:50:49 +0400

    Initial Commit

---
+++ CoinUsers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoinUsers
+//
+//  Created by Hovak Davtyan on 10.05.21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view.
+	}
+
+
+}
+
